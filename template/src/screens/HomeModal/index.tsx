afd9a5928fdca97eb9014aa1b578741535a78b88
Henry Paulino <corasan.dev@gmail.com>
2023-02-21 20:15:38 -0500

    update

--- template/src/screens/HomeModal/index.tsx
+++ template/src/screens/HomeModal/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <View style={styles.container}>
       <PhoneSVG height={220} width={220} />
       <Text style={styles.title}>
-        This is <Text style={styles.indigo}>BottomTabs</Text> template
+        This is a React Navitation <Text style={styles.indigo}>Modal</Text>
       </Text>
       <Text style={styles.subtext}>
         Go to <Text style={styles.code}> src/screens/HomeModal/index.js </Text> to edit
